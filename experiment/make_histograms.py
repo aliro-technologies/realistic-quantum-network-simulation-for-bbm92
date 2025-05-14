5a1198e0bc35a2e32e52923f1663c7f9434896e9
Michelle Chalupnik <mvchalupnik@gmail.com>
2025-05-14 15:15:35 -0400

    Rename delete and clean experiment files

---
+++ experiment/make_histograms.py
@@ -0,0 +1,261 @@
+import numpy as np
+import csv
+import matplotlib.pyplot as plt
+import json
+from scipy.optimize import curve_fit
+import argparse
+import os
+import re
+
+from get_secure_key_rate_error import get_secure_key_rate_error
+
+"""
+To run: 
+`python experiment/make_histograms.py -f [FILE_NAME]`
+where FILE_NAME is the name of the csv file (without the .csv extension)
+containing Swabian data.
+
+Example usage:
+python experiment/make_histograms.py -f RUN_5_BBM92_TIMETAG_STREAM__16HIST_UBENCHPC_12345678_DAHVDAHV_Jan_30
+"""
+
+
+def plot_histograms(
+    simulation_results_folder_path,
+    plot_folder_name,
+    file_name,
+    coincidence_window,
+    approx_delay=0,
+    save_plot=False,
+    same_basis_detectors=True,
+):
+    """
+    This function analyzes data by fitting Gaussians to histograms of coincidences in the streamed Swabian data.
+    This requires the user to manually input the approximate delay between detectors `approx_delay` in picoseconds,
+    the (half) array size in picoseconds to look for delays over, `half_array_size_in_picoseconds`, and the
+    bin size (in picoseconds) `bin_size_in_picoseconds`. From this information, accurate delays and detection resolution
+    (detector jitters, etc) can be computed. Finally, a histogram is computed between all of the coincidences to find the detection
+    resolution AFTER the calculated delays have been subtracted.
+
+    :param simulation_results_folder_path: Folder path name with data.
+    :param plot_folder_name: Folder path name to save plots.
+    :param file_name: Data file name.
+    :param coincidence_window: The time bin size, in picoseconds. Warning: do not exceed 1000 ps.
+    :param approx_delay: The delay to add to Bob's detectors, in picoseconds.
+        Should be 10000 for runs 3, 4 and 15000 for runs 5, 7, 8.
+    :param save_plot: If true, save plots.
+    :param same_basis_detectors: If True, make histograms for same basis detector pairs.
+        If False, make histograms for opposite basis detector pairs.
+    """
+
+    # Load in the data; increase csv file load size to avoid errors
+    max_int = 131072000
+    csv.field_size_limit(max_int)
+
+    with open(
+        simulation_results_folder_path + file_name + ".csv", newline=""
+    ) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data_dict = row
+
+    # Get acquisition time and convert to seconds
+    acq_time_in_seconds = float(data_dict["AcqTime"]) * 1e-12
+
+    # Find the detector ordering in the saved title
+    match = re.search(r"12345678_([^_]{8})_", file_name)
+    if match:
+        detector_map_string = match.group(1)
+        if detector_map_string == "HVDAHVDA":
+            alice_detector_to_bit = {1: 1, 2: 0, 3: 0, 4: 1}
+            bob_detector_to_bit = {5: 0, 6: 1, 7: 0, 8: 1}
+            basis0 = "HV"
+            basis1 = "AD"
+        elif detector_map_string == "DAHVDAHV":
+            alice_detector_to_bit = {1: 0, 2: 1, 3: 1, 4: 0}
+            bob_detector_to_bit = {5: 0, 6: 1, 7: 0, 8: 1}
+            basis0 = "AD"
+            basis1 = "HV"
+        else:
+            raise ValueError(
+                "Invalid title string does not contain basis information in correct format."
+            )
+    else:
+        raise ValueError("Invalid title string does not contain basis information.")
+
+    # In the case where the data time length does not match the recorded Swabian acq time due to
+    # file size issues, find the real aquisition time.
+    for j in range(1, 9):
+        channel_json = data_dict[f'Ch{j}_stream']
+        timestamps = json.loads(channel_json)
+        if j == 1:
+            min_timestamp = timestamps[0]
+            max_timestamp = timestamps[-1]
+        else:
+            min_timestamp = min(min_timestamp, timestamps[0])
+            max_timestamp = max(max_timestamp, timestamps[-1])
+    total_time_in_seconds = (max_timestamp - min_timestamp)*1e-12
+    acq_time_in_seconds = total_time_in_seconds
+
+    if save_plot:
+        # Start by plotting histograms between each set of detectors and finding delays
+        num_rows = 2
+        num_cols = 4
+        fig, axes = plt.subplots(num_rows, num_cols, figsize=(12, 6))
+        axes = axes.flatten()
+
+    # Histogram pairs
+    if same_basis_detectors:
+        hist_pairs = [(4, 8), (4, 7), (3, 7), (3, 8), (2, 5), (2, 6), (1, 6), (1, 5)]
+    else:
+        hist_pairs = [(1, 7), (2, 8), (3, 6), (4, 5), (1, 8), (2, 7), (3, 5), (4, 6)]
+    fitted_delay = {}
+
+    # Define a Gaussian function
+    def gaussian(x, A, mu, sigma):
+        return A * np.exp(-((x - mu) ** 2) / (2 * sigma**2))
+
+    detector_st_devs = []
+
+    for i, hist_pair in enumerate(hist_pairs):
+        half_array_size_in_picoseconds = 6000
+        bin_size_in_picoseconds = 30
+
+        channel1_json = data_dict[f"Ch{hist_pair[0]}_stream"]
+        channel2_json = data_dict[f"Ch{hist_pair[1]}_stream"]
+        d1_timestamps = json.loads(channel1_json)
+        d2_timestamps = json.loads(channel2_json)
+
+        p1, p2 = 0, 0
+        d1_common_basis_choice_times = []
+        d2_common_basis_choice_times = []
+
+        while p1 < len(d1_timestamps) and p2 < len(d2_timestamps):
+            # If the basis choices match and the timestamps are close enough, add to the common_basis_choice_times list
+            adjusted_d2_timestamp = d2_timestamps[p2] + approx_delay
+            timestamp_difference = d1_timestamps[p1] - adjusted_d2_timestamp
+
+            # Compute histogram; cutoff at approx_delay plus coincidence_window
+            if abs(timestamp_difference) < half_array_size_in_picoseconds:
+                d1_common_basis_choice_times.append(d1_timestamps[p1])
+                d2_common_basis_choice_times.append(adjusted_d2_timestamp)
+                p1 += 1
+                p2 += 1
+            elif timestamp_difference < 0:
+                p1 += 1
+            else:
+                p2 += 1
+
+        # Find differences between coincidence timesteps
+        timestamp_differences = np.array(d1_common_basis_choice_times) - np.array(
+            d2_common_basis_choice_times
+        )
+
+        delay_data = np.arange(
+            -int(half_array_size_in_picoseconds),
+            int(half_array_size_in_picoseconds) + bin_size_in_picoseconds,
+            bin_size_in_picoseconds,
+        )
+        half_array_length = int(
+            half_array_size_in_picoseconds / bin_size_in_picoseconds
+        )
+        hist_data = np.zeros(2 * half_array_length + 1)
+
+        # Turn list of time differences into an ordered list of delays (x) and a list of frequency of each delay (y)
+        for timestamp_diff in timestamp_differences:
+            # Round to 10 ps
+            rounded_timestamp_diff = (
+                round(timestamp_diff / bin_size_in_picoseconds)
+                * bin_size_in_picoseconds
+            )
+            rounded_timestamp_diff_index = int(
+                rounded_timestamp_diff * half_array_length / half_array_size_in_picoseconds
+            )
+            rounded_timestamp_diff_index += int(
+                half_array_size_in_picoseconds / bin_size_in_picoseconds
+            )
+            hist_data[rounded_timestamp_diff_index] += 1
+
+        # Extract the delays between pairs of detectors
+        # Fit the data to a Gaussian curve
+        A_estimate = 10
+        mu_estimate = 1000
+        sigma_estimate = 1000
+        popt, pcov = curve_fit(
+            gaussian,
+            delay_data,
+            hist_data,
+            p0=[A_estimate, mu_estimate, sigma_estimate],
+        )
+        A_fit, mu_fit, sigma_fit = popt
+        detector_st_devs.append(sigma_fit)
+
+        fitted_delay[hist_pair] = mu_fit + approx_delay
+
+        errs = np.sqrt(np.diag(pcov))
+        A_err, mu_err, sigma_err = errs
+
+        if A_err > np.sqrt(A_fit):
+            raise ValueError("Fit is bad; modify p0 parameters")
+
+        fine_grained_delay_data = np.linspace(delay_data[0], delay_data[-1], 1000)
+
+        if save_plot:
+            fig.suptitle(
+                f"Acquisition time: {acq_time_in_seconds} seconds; Added delay: {approx_delay} ps"
+            )
+            ax = axes[i]
+            ax.hist(delay_data[:-1], delay_data, weights=hist_data[:-1])
+            ax.set_title(f"Detector pair {hist_pair}")
+            ax.set_ylabel("Coinc. counts")
+            ax.set_xlabel("Picoseconds")
+
+            ax.grid(True)
+            ax.plot(
+                fine_grained_delay_data,
+                gaussian(fine_grained_delay_data, A_fit, mu_fit, sigma_fit),
+                label=f"$\mu={mu_fit:.0f} \pm {mu_err:.0f}$\n"
+                f"$\sigma={sigma_fit:.0f} \pm {sigma_err:.0f}$",
+            )
+            ax.legend(prop={"size": 8})
+
+    print(
+        f"Avg standard deviation (detector RMS jitter): {np.mean(detector_st_devs)} ps"
+    )
+
+    if not same_basis_detectors:
+        file_name += "_not_same_basis"
+    if save_plot:
+        plt.tight_layout()
+        plt.savefig(
+            plot_folder_name + file_name + "_histograms.png", dpi=300
+        )
+
+if __name__ == "__main__":
+    """
+    Run the analysis.
+    """
+    # Parse in file name
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file-name", help="File name with simulation results")
+    args = parser.parse_args()
+
+    data_folder_name = "timetagged_data/"
+    plot_folder_name = "experiment_bbm92_plots/"
+
+    # Create folders
+    if not os.path.isdir(data_folder_name):
+        os.mkdir(data_folder_name)
+
+    if not os.path.isdir(plot_folder_name):
+        os.mkdir(plot_folder_name)
+
+    plot_histograms(
+        simulation_results_folder_path=data_folder_name,
+        plot_folder_name=plot_folder_name,
+        file_name=args.file_name,
+        coincidence_window=1000,
+        approx_delay=15000,
+        save_plot=True,
+        same_basis_detectors=True,
+    )
---
+++ experiment/run_exp_coinc_window_sweep.py
@@ -0,0 +1,341 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+import argparse
+import os
+
+from analyze_data import analyze_data
+from utilities import write_json, datetime_now_string
+
+"""
+To run: 
+`python experiment/run_exp_coinc_window_sweep.py -f [FILE_NAME] -dca [DARK_COUNTS_A] -dcb [DARK_COUNTS_B]`
+where FILE_NAME is the name of the csv file (without the .csv extension)
+containing Swabian data. Average dark counts per second are needed to get an accurate estimate of the source brightness.
+
+If no dark count rates are supplied by user, the default value `DEFAULT_DARK_COUNTS` is taken.
+
+Example:
+python experiment/run_exp_coinc_window_sweep.py -f Mar_20_RUN_1_0P8vTHRESHOLD_BBM92_TIMETAG_STREAM_16HIST_UBENCHPC_12345678_DAHVDAHV_bin_width_1ns_4_35pm -dca 500 -dcb 1800
+"""
+
+DEFAULT_DARK_COUNTS = 1000 # Set default dark count estimate in case dark counts are not supplied
+APPROX_DELAY = 15000 # Set the approximate delay, in picoseconds
+matplotlib.rcParams.update({"font.size": 12})
+
+def run_sweep(
+    coincidence_windows, simulation_results_folder_path, plot_folder_name, file_name
+):
+    """
+    Analyze saved experimental data and find QBERs, secure key rates, raw key rates, and singles rates.
+    Sweep the analysis over different coincidence windows.
+
+    :param coincidence_windows: List of coincidence windows to sweep over (in picoseconds).
+    :param simulation_results_folder_path: Simulation results folder path.
+    :param plot_folder_name: Plot folder path.
+    :param file_name: data file path.
+    """
+    qbers = []
+    qber_errors = []
+    raw_key_rates = []
+    raw_key_rate_errors = []
+    secure_key_rates = []
+    secure_key_rate_errors = []
+    singles_rates_A = []
+    singles_rates_B = []
+
+    for coincidence_window in coincidence_windows:
+        (
+            qber,
+            qber_error,
+            raw_key_rate,
+            raw_key_rate_error,
+            secure_key_rate,
+            secure_key_rate_error,
+            singles_rate_A,
+            singles_rate_B,
+        ) = analyze_data(
+            simulation_results_folder_path,
+            plot_folder_name,
+            file_name,
+            coincidence_window,
+            approx_delay=APPROX_DELAY,
+        )
+        qbers.append(qber)
+        qber_errors.append(qber_error)
+        raw_key_rates.append(raw_key_rate)
+        raw_key_rate_errors.append(raw_key_rate_error)
+        secure_key_rates.append(secure_key_rate)
+        secure_key_rate_errors.append(secure_key_rate_error)
+        singles_rates_A.append(singles_rate_A)
+        singles_rates_B.append(singles_rate_B)
+
+    return (
+        qbers,
+        qber_errors,
+        raw_key_rates,
+        raw_key_rate_errors,
+        secure_key_rates,
+        secure_key_rate_errors,
+        singles_rates_A,
+        singles_rates_B
+    )
+
+
+def plot_and_fit_source_brightness(
+    coincidence_windows,
+    raw_key_rates,
+    singles_rates_A,
+    singles_rates_B,
+    dark_count_rate_A,
+    dark_count_rate_B,
+    plot_folder_name,
+    file_name,
+):
+    """
+    Plot and fit the source internal brightness from the singles rates and coincidence rates, for different
+    coincidence windows. Coincidence to accidental ratio, CAR, = B * total_loss_A * total_loss_B * t_cc /
+    (B * total_loss_A * t_cc * B * total_loss_B * t_cc) = C_r(t_cc) / (S_A * S_B * t_cc) = 1 / (B * t_cc). 
+    where B is the internal source brightness, total_loss_A is the fractional total loss on the link from the
+    source to Alice, total_loss_B is the fractional total loss on the link from the source to Bob, t_cc is 
+    the coincidence window (in seconds), C_r(t_cc) is the coincidence rate, which is a function of t_cc, 
+    S_A is the singles rate on Alice, and S_B is the singles rate on Bob.
+
+    This is also equation A1 in Neumann PRA 104, 022406 (2021).
+
+    :param coincidence_windows: A list of coincidence windows (in seconds).
+    :param raw_key_rates: A list of raw key rates for each coincidence window (in cps).
+    :param singles_rates_A: A list of singles rate on Alice (in cps).
+    :param singles_rates_B: A list of singles rate on Bob (in cps).
+    :param dark_count_rate_A: The dark count rate on Alice (in cps).
+    :param dark_count_rate_B: The dark count rate on Bob (in cps).
+    :param plot_folder_name: The folder name for plots.
+    :param file_name: The file name to use for the saved plot.
+    """
+    coincidence_windows = np.array(coincidence_windows)
+
+    # Find C_R as 2 * raw_key_rates
+    coincidence_rates = np.array(raw_key_rates) * 2
+
+    # Find source internal brightness as S_A * S_B / C_R
+    true_singles_rates_A = np.array(singles_rates_A) - (np.ones(len(singles_rates_A)) * dark_count_rate_A)
+    true_singles_rates_B = np.array(singles_rates_B) - (np.ones(len(singles_rates_B)) * dark_count_rate_B)
+    brightnesses = true_singles_rates_A * true_singles_rates_B / np.array(coincidence_rates)
+
+    ### MANUALLY ADJUST LIMITS TO FIT LINE TO IF FIT FAILS
+    lower_limit_coinc_window = 3e-9
+    upper_limit_coinc_window = 20e-9
+
+    lower_limit_index = (np.abs(coincidence_windows - lower_limit_coinc_window)).argmin()
+    upper_limit_index = (np.abs(coincidence_windows - upper_limit_coinc_window)).argmin()
+
+    coincidence_windows_trunc = coincidence_windows[lower_limit_index:upper_limit_index]
+    brightnesses_trunc = brightnesses[lower_limit_index:upper_limit_index]
+
+    # Define a horizontal line function
+    def horizontal_line(x, A):
+        return A
+
+    # Fit the data to a horizontal line
+    A_estimate = 1.56e6
+    popt, pcov = curve_fit(horizontal_line, coincidence_windows_trunc, brightnesses_trunc, p0=[A_estimate])
+    A_fit = popt
+    errs = np.sqrt(np.diag(pcov))
+    A_err = errs
+
+    # Plot
+    fig, axs = plt.subplots(1)
+    colors = plt.cm.get_cmap("Pastel1")
+
+    p1 = axs.plot(
+        coincidence_windows,
+        brightnesses,
+        color=colors(0),
+        label=f"Estimated source brightness",
+        linewidth=2,
+    )
+
+    fit_coinc_windows = np.linspace(coincidence_windows_trunc[0], coincidence_windows_trunc[-1], 500)
+    p2 = axs.errorbar(
+        fit_coinc_windows,
+        np.ones(len(fit_coinc_windows)) * A_fit,
+        np.ones(len(fit_coinc_windows)) * A_err,
+        color=colors(1),
+        linewidth=2,
+        label=f"Fit line: $B = {A_fit[0]:.2e} \\pm {A_err[0]:.2e}$ cps",
+    )
+    plt.legend()
+
+    plt.xscale("log")
+    plt.yscale("log")
+
+    axs.set_xlabel("Time stamp bin size (seconds)")
+    axs.set_ylabel("Source brightness (counts per second)")
+    plt.tight_layout()
+
+    plt.savefig(
+        f"{plot_folder_name}{file_name}_internal_source_brightness_final.png",
+        dpi=300,
+    )
+    print(f"Estimated source brightness\n$S_A = {singles_rates_A[0]:.2e}$ cps \n$S_B = {singles_rates_B[0]:.2e}$ cps")
+
+
+def plot_qbers_and_raw_key_rates(
+    x,
+    raw_key_rates,
+    raw_key_rate_errors,
+    qbers,
+    qber_errors,
+    plot_folder_name,
+    file_name,
+):
+    """
+    Plot qbers and raw key rates.
+
+    :param x: parameter to plot on the x axis.
+    :param raw_key_rates: The raw key rate.
+    :param raw_key_rate_errors: the raw key rate errors.
+    :param qbers: The qbers.
+    :param qber_errors: The qber errors.
+    :param plot_folder_name: The folder name for plots.
+    :param file_name: The file name to use for the saved plot.
+    """
+    matplotlib.rcParams.update({"font.size": 16})
+
+    # Plot results
+    fig, axs = plt.subplots(1)
+    colors = plt.cm.get_cmap("Set1")
+
+    p1 = axs.errorbar(
+        np.array(x),
+        np.array(raw_key_rates),
+        np.array(raw_key_rate_errors),
+        capsize=5,
+        capthick=1,
+        color=colors(0),
+        label="Raw key rate",
+        linestyle='',
+    )
+
+    ax2 = axs.twinx()
+    p2 = ax2.errorbar(
+        np.array(x),
+        qbers,
+        qber_errors,
+        capsize=5,
+        capthick=1,
+        color=colors(1),
+        label="QBER",
+        linestyle='',
+    )
+
+    axs.yaxis.label.set_color(colors(0))
+    ax2.yaxis.label.set_color(colors(1))
+    axs.tick_params(axis="y", colors=colors(0))
+    ax2.tick_params(axis="y", colors=colors(1))
+
+    axs.ticklabel_format(axis="y", style="sci", scilimits=(0, 0))
+
+    axs.set_xlabel("Time stamp bin size (seconds)")
+    axs.set_ylabel("Raw key rate (bits per second)")
+    axs.set_ylim(bottom=0)
+    ax2.set_ylabel("QBER")
+    ax2.set_ylim(bottom=0, top=0.5)
+    plt.xscale("log")
+    plt.tight_layout()
+
+    plt.savefig(
+        f"{plot_folder_name}{file_name}_qber_vs_time_stamp_final.png",
+        dpi=300,
+    )
+
+
+if __name__ == "__main__":
+    """
+    Run the parameter sweep and plot.
+    """
+    # Parse in file name
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file-name", help="File name with simulation results")
+    parser.add_argument("-dca", "--dark-count-a", help="Dark count rate on Alice (all detectors), in cps")
+    parser.add_argument("-dcb", "--dark-count-b", help="Dark count rate on Bob (all detectors), in cps")
+
+    args = parser.parse_args()
+    if args.dark_count_a:
+        dark_count_rate_A = float(args.dark_count_a)
+    else:
+        dark_count_rate_A = DEFAULT_DARK_COUNTS
+
+    if args.dark_count_b:
+        dark_count_rate_B = float(args.dark_count_b)
+    else:
+        dark_count_rate_B = DEFAULT_DARK_COUNTS
+
+    data_folder_name = "timetagged_data/"
+    plot_folder_name = "experiment_bbm92_plots/"
+    data_file_name = datetime_now_string()
+
+    # Create folders
+    if not os.path.isdir(data_folder_name):
+        os.mkdir(data_folder_name)
+
+    if not os.path.isdir(plot_folder_name):
+        os.mkdir(plot_folder_name)
+
+    # Make list of coincidence windows, in picoseconds
+    coincidence_windows = np.logspace(1, 7, 150)
+
+    # Convert to seconds
+    coincidence_windows_in_seconds = [
+        coincidence_window * 1e-12 for coincidence_window in coincidence_windows
+    ]
+
+    qbers, qber_errors, raw_key_rates, raw_key_rate_errors, secure_key_rates, secure_key_rate_errors, singles_rates_A, singles_rates_B= run_sweep(
+        coincidence_windows, data_folder_name, plot_folder_name, args.file_name
+    )
+    # Create a dict with results
+    experiment_analysis = {}
+    experiment_analysis["data_type"] = "experiment"
+    experiment_analysis["data_file_name"] = args.file_name
+    experiment_analysis["coincidence_window"] = [cw*1e-12 for cw in coincidence_windows]
+    experiment_analysis["secure_key_rates"] = secure_key_rates
+    experiment_analysis["secure_key_rate_errors"] = secure_key_rate_errors
+    experiment_analysis["raw_key_rates"] = raw_key_rates
+    experiment_analysis["raw_key_rate_errors"] = raw_key_rate_errors
+    experiment_analysis["qbers"] = qbers
+    experiment_analysis["qber_errors"] = qber_errors
+
+    experiment_analysis["dark_count_rate_A"] = dark_count_rate_A
+    experiment_analysis["dark_count_rate_B"] = dark_count_rate_B
+
+    experiment_analysis["qbers_label"] = "QBER"
+    experiment_analysis["raw_key_rates_label"] = "Raw key rate (bps)"
+    experiment_analysis["secure_key_rates_label"] = "Secure key rate (bps)"
+    experiment_analysis["x_parameter_name"] = "coincidence_window"
+    experiment_analysis["x_parameter_label"] = "Coincidence window (s)"
+
+    # Save dict with parameters and sim results to JSON file
+    write_json(experiment_analysis, plot_folder_name + data_file_name + "_experimental_analysis")
+
+    # Plot and fit the source brightness.
+    plot_and_fit_source_brightness(
+        coincidence_windows=coincidence_windows_in_seconds,
+        raw_key_rates=raw_key_rates,
+        singles_rates_A=singles_rates_A,
+        singles_rates_B=singles_rates_B,
+        dark_count_rate_A=dark_count_rate_A,
+        dark_count_rate_B=dark_count_rate_B,
+        plot_folder_name=plot_folder_name,
+        file_name=args.file_name,
+    )
+    # Make plot and save
+    plot_qbers_and_raw_key_rates(
+        x=coincidence_windows_in_seconds,
+        raw_key_rates=raw_key_rates,
+        raw_key_rate_errors=raw_key_rate_errors,
+        qbers=qbers,
+        qber_errors=qber_errors,
+        plot_folder_name=plot_folder_name,
+        file_name=args.file_name,
+    )
